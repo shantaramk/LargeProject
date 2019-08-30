4bce316e4306020014c24a13c71aa2ba94d6b3da
Shantaram K <shantaramk@mipladmin-67.local>
2019-08-30 13:27:14 +0530

    age set to 0 to test the unit test

--- LargeProjectTests/LargeProjectTests.swift
+++ LargeProjectTests/LargeProjectTests.swift
@@ -22,13 +22,10 @@ class LargeProjectTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+        
+        let age = 0
+        
+        XCTAssertTrue(age == 0, "Age can not be 0")
     }
 
 }
